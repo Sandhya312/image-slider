dd6a278dffd4bf3f0badf064a8888b64b05d4972
sandhya <sandhyakumari3122003@gmail.com>
2022-10-31 16:27:57 +0530

    assesment

--- script.js
+++ script.js
@@ -1,39 +1,18 @@
 
-let imgbox = document.querySelector('.imgBox');
-let whiteboxes = document.querySelectorAll('.whiteBox');
-
-imgbox.addEventListener('dragstart',(e)=>{
-  console.log('start');
-  e.target.className += ' hold';
-  setTimeout(() => {
-    e.target.className =  'hide';
-  }, 0);
-})
-imgbox.addEventListener('dragend',(e)=>{
-  console.log('end');
-  e.target.className = 'imgBox';
+$(function() {
+    $( "#imageListId" ).sortable({
+    update: function(event, ui) {
+        getIdsOfImages();
+    }//end update         
+    });
 });
-
-
-for( boxes of whiteboxes){
-   boxes.addEventListener('dragenter',(e)=>{
-     console.log('enter');
-     e.target.className += ' dashed';
-   })
-
-   boxes.addEventListener('dragover',(e)=>{
-     console.log('over')
-     e.preventDefault();
-   })
-
-   boxes.addEventListener('dragleave',(e)=>{
-     console.log('leave')
-   e.target.className = 'whiteBox';
-   })
-
-   boxes.addEventListener('drop',(e)=>{
-     console.log('drop');
-     e.target.append(imgbox);
-   })
-
+  
+function getIdsOfImages() {
+    var values = [];
+    $('.listitemClass').each(function (index) {
+        values.push($(this).attr("id")
+                .replace("imageNo", ""));
+    });
+      
+    $('#outputvalues').val(values);
 }
